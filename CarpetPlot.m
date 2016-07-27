53e3a8edd068c89f140534c81b6758dfec0b8c9f
Thomas M. Hermann <thomas.m.hermann@odonata-research.com>
2016-07-27 11:36:09 -0500

    Increase the maximum points for interpolation to 50.

--- CarpetPlot.m
+++ CarpetPlot.m
@@ -800,7 +800,7 @@ classdef CarpetPlot < handle
 
             % Constants
             obj.CONTOUR_RESOLUTION = 250;
-            obj.MAX_POINTS = 25;
+            obj.MAX_POINTS = 50;
 
             % Set default labels: The variable input names
             obj.needRelabel = 0;
